1100e95886ee767cd4ba1796b01dbd9583562538
Joshua golding <golding.joshua8@gmail.com>
2025-12-26 23:12:16 -0500

    common package

---
+++ common/leds.h
@@ -0,0 +1,32 @@
+#ifndef leds_h
+#define leds_h
+
+//TODO: replace with real values
+#define PLAYER1_BTN1 0
+#define PLAYER1_BTN2 2
+#define PLAYER1_BTN3 4
+#define PLAYER1_BTN4 6
+#define PLAYER1_BTN5 8
+#define PLAYER1_BTN6 10
+#define PLAYER1_BTN7 12
+#define PLAYER1_BTN8 14
+// #define PLAYER1_JOYSTICK L3;
+
+//TODO: replace with real values
+#define PLAYER2_BTN1 16
+#define PLAYER2_BTN2 18
+#define PLAYER2_BTN3 20
+#define PLAYER2_BTN4 22
+#define PLAYER2_BTN5 24
+#define PLAYER2_BTN6 26
+#define PLAYER2_BTN7 28
+#define PLAYER2_BTN8 30
+// #define PLAYER2_JOYSTICK L3;
+
+#define OPTIONS_LED_R 16
+#define OPTIONS_LED_G 18
+#define OPTIONS_LED_B 20
+
+//TODO: replace with real values
+#define BACKLIGHT_TOP BLT
+#endif
