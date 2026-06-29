bdd42ac7909c31ea178e675d7ed0daa5b1d12851
Joshua Golding <golding.joshua8@gmail.com>
2026-06-29 15:21:10 +0000

    Cap maximum brightness to limit unnecessary power draw

--- include/brightness_levels.h
+++ include/brightness_levels.h
@@ -1,7 +1,7 @@
 #ifndef brightness_levels_h
   #define brightness_levels_h
   #define BRIGHTNESS_OFF 0
-  #define BRIGHTNESS_LOW 33
-  #define BRIGHTNESS_MEDIUM 66
-  #define BRIGHTNESS_MAX 100
+  #define BRIGHTNESS_LOW 25
+  #define BRIGHTNESS_MEDIUM 50
+  #define BRIGHTNESS_MAX 75
 #endif
