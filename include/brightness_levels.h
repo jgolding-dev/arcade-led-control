36b25dd605c86b48e1cd5e4c10bcac684fae83d5
Joshua Golding <golding.joshua8@gmail.com>
2026-06-29 23:16:41 -0400

    Change brightness cap

--- include/brightness_levels.h
+++ include/brightness_levels.h
@@ -1,7 +1,7 @@
 #ifndef brightness_levels_h
   #define brightness_levels_h
   #define BRIGHTNESS_OFF 0
-  #define BRIGHTNESS_LOW 25
-  #define BRIGHTNESS_MEDIUM 50
-  #define BRIGHTNESS_MAX 75
+  #define BRIGHTNESS_LOW 30
+  #define BRIGHTNESS_MEDIUM 60
+  #define BRIGHTNESS_MAX 90
 #endif
